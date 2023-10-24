782451cafb1c095fd9d977a69294eea7d9fa4284
Tao89116 <147602994+Tao89116@users.noreply.github.com>
2023-10-24 17:34:56 +0800

    Add files via upload

---
+++ lab4.js
@@ -0,0 +1,43 @@
+
+    function changecolor(){
+        var btn=document.getElementById("table");
+        btn.style.background="cyan";
+    }
+    function changecolor1(){
+        var btn=document.getElementById("table");
+        btn.style.background="green";
+    }
+    function changecolor2(){
+        var btn=document.getElementById("table");
+        btn.style.background="purple";
+    }
+    function changecolor3(){
+        var btn=document.getElementById("table");
+        btn.style.background="orange";
+    }
+    function changeborder(){
+        var btn=document.getElementById("table");
+        btn.style.borderSpacing="1px";
+    }
+    function changeborder1(){
+        var btn=document.getElementById("table");
+        btn.style.borderSpacing="10px";
+    }
+    function changeborder2(){
+        var btn=document.getElementById("table");
+        btn.style.borderSpacing="20px";
+    }
+    function changeTableWidth(){
+        var btn=document.getElementById("table")
+        btn.style.width="200px";
+    }
+    function changeTableWidth1(){
+        var btn=document.getElementById("table")
+        btn.style.width="500px";
+    }
+    function reset(){
+        var btn=document.getElementById("table")
+        btn.style.width="100px";
+        btn.style.borderSpacing="2px";
+        btn.style.backgroundColor="white";
+    }
